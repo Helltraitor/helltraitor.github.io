0075318a975aeb823fded826b3d64d2dd9fbc792
Helltraitor <helltraitor@hotmail.com>
2022-10-27 20:38:23 +0300

    Update HeaderNavigationDesktop.vue

--- src/components/Header/HeaderNavigationDesktop.vue
+++ src/components/Header/HeaderNavigationDesktop.vue
@@ -9,59 +9,65 @@ const toggleLocales = () => {
 </script>
 
 <template>
-  <nav id="headerDesktopNavigation">
-    <RouterLink
-      to="/blog"
-      class="icon-btn"
-    >
-      Blog
-    </RouterLink>
-    <a
-      href="https://github.com/helltraitor"
-      class="icon-btn"
-    >
-      Github
-    </a>
-    <a
-      href="https://github.com/helltraitor/helltraitor.github.io"
-      class="icon-btn"
-    >
-      Source
-    </a>
-    <a
-      href="mailto: helltraitor@hotmail.com"
-      class="icon-btn"
-    >
-      Mail
-    </a>
-    <button
-      class="icon-btn"
-      :title="t('button.toggle_dark')"
-      @click="toggleDark()"
-    >
-      <div i="carbon-sun dark:carbon-moon" />
-    </button>
-    <button
-      class="icon-btn"
-      :title="t('button.toggle_langs')"
-      @click="toggleLocales()"
-    >
-      <div i-carbon-language />
-    </button>
-  </nav>
+  <div>
+    <nav id="headerDesktopNavigation">
+      <RouterLink
+        to="/blog"
+        class="icon-btn"
+      >
+        Blog
+      </RouterLink>
+      <a
+        href="https://github.com/helltraitor"
+        class="icon-btn"
+      >
+        Github
+      </a>
+      <a
+        href="https://github.com/helltraitor/helltraitor.github.io"
+        class="icon-btn"
+      >
+        Source
+      </a>
+      <a
+        href="mailto: helltraitor@hotmail.com"
+        class="icon-btn"
+      >
+        Mail
+      </a>
+      <button
+        class="icon-btn"
+        :title="t('button.toggle_dark')"
+        @click="toggleDark()"
+      >
+        <div i="carbon-sun dark:carbon-moon" />
+      </button>
+      <button
+        class="icon-btn"
+        :title="t('button.toggle_langs')"
+        @click="toggleLocales()"
+      >
+        <div i-carbon-language />
+      </button>
+    </nav>
+  </div>
 </template>
 
 <style scoped lang="sass">
-nav#headerDesktopNavigation
-  align-self: center
-  margin-block: auto
+div
+  display: flex
+  margin: auto
 
-  font-size: 18px
+  nav#headerDesktopNavigation
+    align-self: center
+    margin-block: auto
 
-  display: grid
-  grid-auto-flow: column
-  gap: 20px
+    font-size: 18px
 
-  & > *
-    margin-inline: auto
+    display: grid
+    grid-auto-flow: column
+    gap: 20px
+
+    & > *
+      margin-inline: auto
 </style>
