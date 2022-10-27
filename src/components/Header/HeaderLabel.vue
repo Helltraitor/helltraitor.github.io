26f80edbb4f0bd74979ea37ca71a10b7e476536c
Helltraitor <helltraitor@hotmail.com>
2022-10-27 20:37:55 +0300

    Create HeaderLabel.vue

---
+++ src/components/Header/HeaderLabel.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <RouterLink
+      id="headerLabelValue"
+      to="/"
+      class="icon-btn"
+    >
+      <p>Helltraitor</p>
+    </RouterLink>
+  </div>
+</template>
+
+<style scoped lang="sass">
+div
+  display: flex
+  margin: auto
+
+  p
+    margin: 0
+
+    font-family: 'Courier New', Courier, monospace
+    font-size: 22px
+</style>
