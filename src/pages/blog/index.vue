2419612b8c90f23dd4ccf294990f1c230f8eed37
Helltraitor <helltraitor@hotmail.com>
2022-10-30 02:10:14 +0300

    Added filter by query
    Filterting by input is pending

--- src/pages/blog/index.vue
+++ src/pages/blog/index.vue
@@ -1,9 +1,18 @@
 <!-- eslint-disable curly -->
 <script setup lang="ts">
-import generatedPages from '~pages'
-import { Post } from '~/code/models/post'
+import type { Ref } from 'vue'
+import { parseQuery } from 'vue-router'
 
-const postPages = generatedPages
+import generatedPages from '~pages'
+import { Post, Category as PostCategory } from '~/code/models/post'
+
+/* RECORDS */
+interface IDisplayablePost {
+  post: Post
+  showed: Ref<boolean>
+}
+
+const records: IDisplayablePost[] = generatedPages
   .filter(({ path }) => { return path.includes('/blog/posts/') })
   .map((route) => {
     try {
@@ -15,17 +24,117 @@ const postPages = generatedPages
     }
     return undefined
   })
-  .filter((blogPost) => { return blogPost instanceof Post })
+  .filter((post) => { return post instanceof Post })
   // This map fix type issue with TypeScript
-  .map((blogPost) => { return blogPost as Post })
+  .map((post) => { return post as Post })
+  .map((post) => { return { post, showed: ref(true) } })
+
+/* POSTS BY CATEGORY */
+const categoryRecords: Map<string, IDisplayablePost[]> = new Map()
+for (const record of records) {
+  const category = record.post.meta.category
+  if (!categoryRecords.has(category))
+    categoryRecords.set(category, [])
+
+  categoryRecords.get(category)?.push(record)
+}
+/* ----------------- */
+
+/* POSTS BY LANGUAGE */
+const languageRecords: Map<string, IDisplayablePost[]> = new Map()
+for (const record of records) {
+  const language = record.post.meta.language
+  if (!languageRecords.has(language))
+    languageRecords.set(language, [])
+
+  languageRecords.get(language)?.push(record)
+}
+/* ----------------- */
+
+/* POSTS BY TAG */
+const tagRecords: Map<string, IDisplayablePost[]> = new Map()
+for (const record of records) {
+  for (const tag of record.post.meta.tags) {
+    if (!tagRecords.has(tag))
+      tagRecords.set(tag, [])
+
+    tagRecords.get(tag)?.push(record)
+  }
+}
+/* ------------ */
+
+/* QUERY */
+const query = parseQuery(window.location.search.substring(1))
+
+const queryCategory = Array.isArray(query.category) ? query.category[0] : query.category || ''
+const queryLanguage = Array.isArray(query.language) ? query.language[0] : query.language || ''
+
+const queryTags = (Array.isArray(query.tag) ? query.tag : [])
+  .map((value) => { return value as string })
+/* ----- */
+
+/* REFERENCES */
+const requiredCategory = ref(
+  Object.values(PostCategory)
+    .includes(queryCategory as PostCategory)
+    ? queryCategory as string
+    : null,
+)
+const requiredLanguage = ref(queryLanguage || null)
+const requiredTags = ref(new Set(queryTags))
+/* ---------- */
+
+/* UPDATING */
+const updatePostShowedRef = () => {
+  records.forEach(({ showed }) => showed.value = true)
+
+  if (requiredCategory.value) {
+    records.forEach(({ post, showed }) => {
+      showed.value = post.meta.category as string === requiredCategory.value
+    })
+  }
+
+  if (requiredLanguage.value) {
+    records
+      .filter(({ showed }) => showed.value)
+      .forEach(({ post, showed }) => {
+        showed.value = post.meta.language as string === requiredLanguage.value
+      })
+  }
+
+  if (requiredTags.value.size !== 0) {
+    records
+      .filter(({ showed }) => showed.value)
+      .forEach(({ post, showed }) => {
+        let mustShow = false
+        for (const tag of post.meta.tags) {
+          if (requiredTags.value.has(tag)) {
+            mustShow = true
+            break
+          }
+        }
+        showed.value = mustShow
+      })
+  }
+}
+/* -------- */
+
+/* WATCHERS */
+watch(requiredCategory, updatePostShowedRef)
+watch(requiredLanguage, updatePostShowedRef)
+watch(requiredTags, updatePostShowedRef)
+
+/* SETUP */
+onMounted(updatePostShowedRef)
 </script>
 
 <template>
   <ul>
-    <li v-for="(post, index) of postPages" :key="index">
+    <li v-for="({ post, showed }, index) of records" :key="index">
       <RouterLink
-        :to="post.route.path"
+        v-show="showed.value"
         class="icon-btn"
+        :to="post.route.path"
       >
         {{ post.meta.name }}
       </RouterLink>
