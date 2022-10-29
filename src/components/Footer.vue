05de12ea549e279fda5b592ba8fdd55e0be28080
Helltraitor <helltraitor@hotmail.com>
2022-10-29 20:43:29 +0300

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,19 +1,45 @@
-<script setup lang="ts">
-const { t } = useI18n()
-</script>
-
 <template>
-  <nav text-xl mt-6>
-    <RouterLink class="icon-btn mx-2" to="/" :title="t('button.home')">
-      <div i-carbon-campsite />
-    </RouterLink>
-
-    <RouterLink class="icon-btn mx-2" to="/about" :title="t('button.about')">
-      <div i-carbon-dicom-overlay />
-    </RouterLink>
-
-    <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank" title="GitHub">
-      <div i-carbon-logo-github />
-    </a>
-  </nav>
+  <footer
+    p-4 m-a
+    flex flex-col
+    opacity-75 dark:opacity-50
+    text-sm
+    text-gray-700 dark:text-gray-200
+  >
+    <div
+      m-a
+      content-center
+    >
+      <div>
+        <p>
+          The source code of this site is under
+          <a href="https://github.com/Helltraitor/helltraitor.github.io/blob/main/LICENSE">
+            <b>MIT License</b>
+          </a>, while its content is under
+          <a href="https://github.com/Helltraitor/helltraitor.github.io/blob/main/CC-BY-SA-4.0">
+            <b>CC BY-SA 4.0</b>
+          </a> license.
+        </p>
+        <p>
+          So it's kinda private, you know? Please, refer to content by link, and so you can freely use it.
+        </p>
+      </div>
+      <div>
+        <p>
+          This site is powered by
+          <a href="https://vitejs.dev">
+            <b>Vite</b>
+          </a>,
+          <a href="https://github.com/antfu/vitesse">
+            <b>Vitesse</b>
+          </a>,
+          <a
+            href="https://vuejs.org"
+          >
+            Vue
+          </a> and other technologies.
+        </p>
+      </div>
+    </div>
+  </footer>
 </template>
