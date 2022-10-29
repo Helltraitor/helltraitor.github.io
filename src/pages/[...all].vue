c39d30f98741ceacb1179ede26f25cf9d568412b
Helltraitor <helltraitor@hotmail.com>
2022-10-29 20:44:49 +0300

    Update [...all].vue

--- src/pages/[...all].vue
+++ src/pages/[...all].vue
@@ -1,14 +1,39 @@
 <script setup lang="ts">
-const { t } = useI18n()
+const location = window.location.toString()
+const githubIssueLocation = 'https://github.com/Helltraitor/helltraitor.github.io/issues/new'
 </script>
 
 <template>
-  <div>
-    {{ t('not-found') }}
+  <div
+    m-a
+    text-center
+  >
+    <div>
+      <p
+        m-2
+        text-4xl
+      >
+        NOT FOUND
+      </p>
+
+      <p m-2>
+        Location is not exist:
+        <a
+          underline
+          :href="location"
+        >{{ location }}
+        </a>
+      </p>
+
+      <p>
+        If this location <b>MUST</b> exist, you can
+        <a
+          underline
+          :href="githubIssueLocation"
+        >
+          report issue
+        </a>
+      </p>
+    </div>
   </div>
 </template>
-
-<route lang="yaml">
-meta:
-  layout: 404
-</route>
