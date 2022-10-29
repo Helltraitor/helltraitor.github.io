b4db12c19592c6a5b9717751d94e95233c9bc9c3
Helltraitor <helltraitor@hotmail.com>
2022-10-29 19:36:18 +0300

    Update default.vue

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,10 +1,17 @@
 <template>
-  <Header />
-  <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
-    <RouterView />
+  <div
+    m-0 p-0
+    h-screen
+    flex flex-col
+  >
+    <Header />
+    <main
+      m-a p-4
+      flex flex-col flex-1
+      text-gray-700 dark:text-gray-200
+    >
+      <RouterView />
+    </main>
     <Footer />
-    <div class="mt-5 mx-auto text-center opacity-75 dark:opacity-50 text-sm">
-      [Default Layout]
-    </div>
-  </main>
+  </div>
 </template>
